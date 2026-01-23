7fa0090f3053c913eb5ae5f9235bf5c136b40f3c
HamishM58 <hamishfmitchell@gmail.com>
2026-01-23 17:22:52 +1100

    Update SRT_OpenCan.h

--- 2026 Season/SRT_OpenCan/SRT_OpenCan.h
+++ 2026 Season/SRT_OpenCan/SRT_OpenCan.h
@@ -10,13 +10,14 @@ private:
     uint8_t _gear_ratio;
     int (*can_send_msg)(uint16_t, uint8_t, uint8_t*, bool);
     int send_sdo_write(uint16_t index, uint8_t sub, uint32_t value, uint8_t size);
-
+    int send_sdo_read(uint16_t index, uint8_t sub, uint8_t size);
 public:
     SRT_CanOpenMtr(int (*sendfunc)(uint16_t, uint8_t, uint8_t*, bool), uint8_t nodeid, uint8_t gearratio);
     int process_msg(uint16_t can_id, uint8_t len, uint8_t* data);
     int enable_motor();
     int move_relative(int32_t steps,uint32_t velocity, uint32_t accel_ms = 1000, uint32_t decel_ms = 1000);
     int move_absolute(int32_t target,uint32_t velocity, uint32_t accel_ms = 1000, uint32_t decel_ms = 1000);
+    bool is_motor_running();
 
     int stop();
     int Estop();
