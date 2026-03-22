aae6b655ea7092926ef78647172de84c407514bc
HamishM58 <hamishfmitchell@gmail.com>
2026-03-22 14:15:40 +1100

    Update SRT_OpenCan.h

--- 2026 Season/SRT_OpenCan/SRT_OpenCan.h
+++ 2026 Season/SRT_OpenCan/SRT_OpenCan.h
@@ -8,12 +8,14 @@ class SRT_CanOpenMtr {
 private:
     uint8_t _node_id;
     uint8_t _gear_ratio;
+    bool _moter_disabled = false;
     int (*can_send_msg)(uint16_t, uint8_t, uint8_t*, bool);
     int send_sdo_write(uint16_t index, uint8_t sub, uint32_t value, uint8_t size);
     int send_sdo_read(uint16_t index, uint8_t sub, uint8_t size);
 public:
     SRT_CanOpenMtr(int (*sendfunc)(uint16_t, uint8_t, uint8_t*, bool), uint8_t nodeid, uint8_t gearratio);
     int process_msg(uint16_t can_id, uint8_t len, uint8_t* data);
+    int reset_alarm();
     int enable_motor();
     int move_relative(int32_t steps,uint32_t velocity, uint32_t accel_ms = 1000, uint32_t decel_ms = 1000);
     int move_absolute(int32_t target,uint32_t velocity, uint32_t accel_ms = 1000, uint32_t decel_ms = 1000);
@@ -23,7 +25,8 @@ public:
     int Estop();
 
     //=========== Homing attempts =========
-    bool home_motor(); // run homing sequence, set current pos = 0
+    bool home_motor(int home_offset); // run homing sequence, does not set homed pos = 0
+    bool set_motor_pos_zero(); // set current pos = 0 without moving, use after homing or manual positioning
 };
 
 #endif
